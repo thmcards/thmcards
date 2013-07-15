b15942d6adfa7cb2589a27e90716e1642c7277b5
dknp40 <daniel.knapp@mni.thm.de>
2013-07-15 23:46:13 +0200

    favicon

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
   //app.use(express.logger());
   app.use(express.cookieParser());
   app.use(express.bodyParser());
