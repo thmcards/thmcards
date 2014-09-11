af02046b154b8eabc70a958f2665eb5e2e2056a9
Kadrei <benedikt.kusemann@freenet.de>
2014-09-11 13:19:21 +0200

    *Added Sonar, Maven and Xvfb Plugin Configuration * Puppet besser sortiert

--- puppet/manifests/trusty32.pp
+++ puppet/manifests/trusty32.pp
@@ -7,67 +7,6 @@ exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
 
-# Install curl
-package { "curl":
-  ensure => "installed"
-}
-
-#package {"build-essential":
-#  ensure => "installed",
-#  provider => apt
-#}
-
-package{"Xvfb":
-  ensure =>"installed"
-}->
-package{"xfonts-scalable":
-  ensure =>"installed"
-}->
-package{"xfonts-cyrillic":
-  ensure =>"installed"
-}->
-package{"xfonts-100dpi":
-  ensure =>"installed"
-}->
-package{"xfonts-75dpi":
-  ensure =>"installed"
-}->
-package{"culmus":
-  ensure =>"installed"
-}->
-package{"imagemagick":
-  ensure =>"installed"
-}
-package { "firefox": ensure => "latest" }
-exec {"install Google Chrome":
-  command => "sudo apt-get install libxss1 libappindicator1 libindicator7 && wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb && sudo dpkg -i google-chrome*.deb"
-}
-
-package{"maven":
-  ensure => "latest"
-}
-
-exec { "install-node":
-  command => "/usr/bin/curl -sL https://deb.nodesource.com/setup | /bin/bash - && /usr/bin/apt-get -y install nodejs"
-}# -> 
-#exec { "npm_use_py2":
-#  command => "npm config set python python2"
-#} -> 
-#exec { "install se-interpreter":
-#  environment => ["HOME=/usr/bin"],
-#  command => "npm install -g se-interpreter",
-#  require => Package["build-essential"]
-#}
-
-#exec { "add-node-repo":
-#  command => "/usr/bin/curl -sL https://deb.nodesource.com/setup | /bin/bash -"
-#}
-## Install nodejs
-#package { "nodejs":
-#  ensure => "installed"
-#}
-
-
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 # Use Mac style installation folder
@@ -81,17 +20,6 @@ file { "/usr/local/bin":
 user { "vagrant":
   ensure => present,
   shell  => "/bin/bash"
-}
-
-# Install couchdb
-package { "couchdb":
-  ensure => "installed"
-}
-
-service { "couchdb":
-  ensure => "running",
-  enable => true,
-  require => Package["couchdb"]
 }
 
 include thmcards
--- puppet/modules/thmcards/manifests/init.pp
+++ puppet/modules/thmcards/manifests/init.pp
@@ -1,5 +1,72 @@
 class thmcards {
 
+  include git
+
+  package{"maven":
+    ensure => "latest"
+  }
+
+  # Install curl
+  package { "curl":
+    ensure => "installed"
+  }
+
+  #package {"build-essential":
+  #  ensure => "installed",
+  #  provider => apt
+  #}
+
+  exec { "install-node":
+    command => "/usr/bin/curl -sL https://deb.nodesource.com/setup | /bin/bash - && /usr/bin/apt-get -y install nodejs",
+    require => Package["curl"]
+  }# -> 
+  #exec { "npm_use_py2":
+  #  command => "npm config set python python2"
+  #} -> 
+  #exec { "install se-interpreter":
+  #  environment => ["HOME=/usr/bin"],
+  #  command => "npm install -g se-interpreter",
+  #  require => Package["build-essential"]
+  #}
+
+  # Install couchdb
+  package { "couchdb":
+    ensure => "installed"
+  }
+
+  service { "couchdb":
+    ensure => "running",
+    enable => true,
+    require => Package["couchdb"]
+  }
+
+  #Headless firefox dependencies
+  package{"Xvfb":
+    ensure =>"installed"
+  }->
+  package{"xfonts-scalable":
+    ensure =>"installed"
+  }->
+  package{"xfonts-cyrillic":
+    ensure =>"installed"
+  }->
+  package{"xfonts-100dpi":
+    ensure =>"installed"
+  }->
+  package{"xfonts-75dpi":
+   ensure =>"installed"
+  }->
+  package{"culmus":
+    ensure =>"installed"
+  }->
+  package{"imagemagick":
+    ensure =>"installed"
+  }
+
+  package { "firefox": 
+    ensure => "latest" 
+  }
+
   file { "/home/vagrant/thmcards-start.sh":
     owner => "vagrant",
     group => "vagrant",
@@ -49,10 +116,7 @@ class thmcards {
       }
   }
 
-  jenkins::job { "thmcards-build-job":
-    name => "THMcards",
-    config_file => "/etc/puppet/files/jenkins/thmcards.config.xml"
-  }
+
 
 # Jenkins might be installed to different paths depending on OS
   $jenkins_home = $::osfamily ? {
@@ -61,9 +125,27 @@ class thmcards {
     default => fail("Unsupported OS family: ${::osfamily}")
   }
 
-  #file { "${jenkins_home}/hudson.tasks.Maven.xml":
-  #  require => Class["jenkins::package"],
-  #  source => "/etc/puppet/files/jenkins/hudson.tasks.Maven.xml",
-  #  notify => Service["jenkins"]
-  #}
+  jenkins::job { "thmcards-build-job":
+    name => "THMcards",
+    config_file => "/etc/puppet/files/jenkins/thmcards.config.xml"
+  }
+
+  file { "${jenkins_home}/hudson.plugins.sonar.SonarPublisher.xml":
+    require => Class["jenkins::package"],
+    source => "/etc/puppet/files/jenkins/hudson.plugins.sonar.SonarPublisher.xml",
+    notify => Service["jenkins"]
+  }
+
+  file { "${jenkins_home}/org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml":
+    require => Class["jenkins::package"],
+    source => "/etc/puppet/files/jenkins/org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml",
+    notify => Service["jenkins"]
+  }
+
+  file { "${jenkins_home}/hudson.tasks.Maven.xml":
+    require => Class["jenkins::package"],
+    source => "/etc/puppet/files/jenkins/hudson.tasks.Maven.xml",
+    notify => Service["jenkins"]
+  }
+
 }
